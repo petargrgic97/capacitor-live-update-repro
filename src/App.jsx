5d44d47c3ada4d156c0b1799f53e6d013e1ae8d4
petargrgic <petargrgic97@gmail.com>
2025-05-21 14:16:19 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, Capacitor + React!</h1>
+    </div>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: './src',
+  root: "./src",
   build: {
-    outDir: '../dist',
+    outDir: "../dist",
     minify: false,
     emptyOutDir: true,
   },
+  plugins: [react()],
 });
